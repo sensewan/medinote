19205d80598fc8bb5e26ecfd683c12c7adfd41d6
82102 <82102@DESKTOP-15MR48J>
2020-12-21 11:20:22 +0900

    jieunii v1.00.17 - USER_SRCH_TAG insert 추가

--- src/main/java/mybatis/dao/HomeDAO.java
+++ src/main/java/mybatis/dao/HomeDAO.java
@@ -30,9 +30,8 @@ public class HomeDAO {
 		StringBuffer sb = new StringBuffer();
 
 		String[] bcd = b_cd.split(",");
-		
+
 		List<String> slist = new ArrayList<String>(Arrays.asList(bcd));
-		
 
 		List<HomeVO> list = sst.selectList("home.bodySymptom", slist);
 
@@ -67,25 +66,50 @@ public class HomeDAO {
 		String u_id = (String) session.getAttribute("loginId");
 
 		List<String> list = sst.selectList("home.getUserSrch", u_id);
-		Collections.sort(list);	//정렬
+		Collections.sort(list); // 정렬
 
 		// 기존 리스트에서 추가 된 증상 비교
 		int cnt = 0;
-		for (String s_cd : s_cdar) {
-			if (!list.contains(s_cd)) {
-				list.add(s_cd);
-				cnt++;
-			}
-		}
 
-		//증상 추가
-		if (cnt > 0) {
+		// USER_SRCH_TAG 테이블에 등록 된 사용자
+		if (list.size() > 0) {
+
+			// 데이터가 null일 경우 list에서 null 삭제
+			if (list.get(0) == null)
+				list.remove(0);
+
+			for (String s_cd : s_cdar) {
+				if (!list.contains(s_cd)) {
+					list.add(s_cd);
+					cnt++;
+				}
+			}
+
+			// 증상 추가
+			if (cnt > 0) {
+				StringBuffer bf = new StringBuffer();
+				for (int i = 0; i < list.size(); i++) {
+					if (i != 0)
+						bf.append(".");
+
+					bf.append(list.get(i));
+				}
+
+				Map<String, String> map = new HashMap<String, String>();
+
+				map.put("u_id", u_id);
+				map.put("srch_tag", bf.toString());
+
+				sst.update("home.updUserSrch", map);
+			}
+		} else {
+			// USER_SRCH_TAG 테이블에 등록되지 않은 사용자
 			StringBuffer bf = new StringBuffer();
-			for (int i = 0; i < list.size(); i++) {
+			for (int i = 0; i < s_cdar.length; i++) {
 				if (i != 0)
 					bf.append(".");
 
-				bf.append(list.get(i));
+				bf.append(s_cdar[i]);
 			}
 
 			Map<String, String> map = new HashMap<String, String>();
@@ -93,14 +117,14 @@ public class HomeDAO {
 			map.put("u_id", u_id);
 			map.put("srch_tag", bf.toString());
 
-			sst.update("home.updUserSrch", map);
+			sst.update("home.insUserSrch", map);
 		}
-		
+
 		return res;
 
 	}
-	
-	public List<String> srchS_NM(String[] s_cdar){
+
+	public List<String> srchS_NM(String[] s_cdar) {
 		List<String> s_nm = sst.selectList("home.srchS_NM", s_cdar);
 		return s_nm;
 	}
